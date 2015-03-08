e22fd89423a31f40e6f5808f12bcddf1c6087ab7
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-03-08 13:13:14 +0900

    トークン変更

--- batch/test_notification.rb
+++ batch/test_notification.rb
@@ -1,7 +1,7 @@
 class TestNotification
   def self.execute
     notification = {
-      device_tokens: ["device_token_one", "device_token_two"],
+      device_tokens: ["edd501209fd91d82d70aa5ce62bf395056f46fb306c2757725eae67910240f42","fd47e21fbdf942f1910567bb36a4a8784a3533662415bfe40d4dfefeecddc6ce"],
       alert: "Something really awesome just happened!",
       sound: "default",
       badge: 1
