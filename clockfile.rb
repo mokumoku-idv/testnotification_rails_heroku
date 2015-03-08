b277d0440aaadba61ec78216943c553fcbdaeac9
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-03-09 00:15:12 +0900

    修正

--- clockfile.rb
+++ clockfile.rb
@@ -1,6 +1,6 @@
 require 'clockwork'
 include Clockwork
 
-every(1.day, 'reminders.send', :at => '00:00') do
-  puts `rails runner -e development batch/test_notification.rb`
+every(1.day, 'pushnotification.send', :at => '00:20') do
+  Dir.chdir("./batch/"){ puts `rails runner -e development test_notification.rb` }
 end
