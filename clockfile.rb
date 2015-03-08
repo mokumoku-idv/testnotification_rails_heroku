8414ffe95c9c1c756b6d7f695d97c77d913d0f29
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-03-08 23:35:48 +0900

    時間変更

--- clockfile.rb
+++ clockfile.rb
@@ -1,6 +1,6 @@
 require 'clockwork'
 include Clockwork
 
-every(1.day, 'reminders.send', :at => '00:30') do
+every(1.day, 'reminders.send', :at => '00:00') do
   puts `rails runner -e development batch/test_notification.rb`
 end
